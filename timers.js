b29dd8fff7b34da7b8f831792d2151b6f6879bb4
The-Killer <killer@righttorule.com>
2020-11-12 20:11:28 -0500

    more data, some text styling

--- timers.js
+++ timers.js
@@ -7,6 +7,7 @@ STFCTimers = (function() {
 
     let mformat = "dddd h a"
     var tieredsystems = {};
+    let NOW = moment()
 
     let systemsLoaded = function(data) {
         console.log(data);
@@ -18,7 +19,8 @@ STFCTimers = (function() {
         Object.keys(STFCTimers.tieredsystems).forEach(tier => {
             $("#syslist").append(`
             <tbody id='t${tier}' class='tier'>
-                <tr><th colspan='4'>Tier ${tier}</th></tr>
+                <tr><th colspan='5'>Tier ${tier}</th></tr>
+                <tr><th>Name</th><th>Direction</th><th>Next Attack</th><th>Power</th><th>Resource</th>
             </tbody>
             `);
             renderSystems(tier)
@@ -29,14 +31,34 @@ STFCTimers = (function() {
         let systems = STFCTimers.tieredsystems[tier];
         systems.forEach(sys =>{
             var time = moment().utc().day(sys.attackDay).hour(sys.attackHour).minute(0)
+            var strtime = time.local().calendar();
             if(moment(time).add(1,"hour").isBefore()) {
                 time.add(1,"week")
+                strtime = time.local().calendar(null,{sameElse:"[Next] dddd [at] h:mm a"})
             }
+            var diff = time.local().diff(NOW,"minutes")
+            var color = null;
+            if(diff < 5) {
+                color = 'blinking'
+            } else if(diff < 120) {
+                color = 'red'
+            } else if(diff < 60*12) {
+                color = 'yellow'
+            }
+
+            if(color != null) {
+                color = " "+color;
+            } else {
+                color = "";
+            }
+
             $("#t"+tier).append(`
-            <tr id='sys-"+sys.name+"' class='sys'>
+            <tr id='sys-"${sys.name}"' class='sys'>
                 <td>${sys.name}</td>
                 <td>${sys.direction}</td>
-                <td class="time">${time.local().calendar()}</td>
+                <td class="time"><span class="${color}">${strtime}</span></td>
+                <td class="power">${sys.power || ""}</td>
+                <td class="power">${sys.resource || ""}</td>
             </tr>`)
         })
     }
