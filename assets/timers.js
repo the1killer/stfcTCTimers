003d980a709b9cc06942875d12e2a2a8f3066d78
The-Killer <killer@righttorule.com>
2020-11-15 23:19:12 -0500

    Make reload on the next 15 min mark

--- assets/timers.js
+++ assets/timers.js
@@ -1,4 +1,18 @@
 // const moment = require('momentjs');
+
+moment.fn.roundNext15Min = function () {
+    var intervals = Math.floor(this.minutes() / 15);
+    if(this.minutes() % 15 != 0)
+        intervals++;
+        if(intervals == 4) {
+            this.add('hours', 1);
+            intervals = 0;
+        }
+        this.minutes(intervals * 15);
+        this.seconds(0);
+        return this;
+    }
+
 let STFCTimers;
 STFCTimers = (function() {
     const init = function() {
